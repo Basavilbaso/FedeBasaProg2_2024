0b270bf19c955a6b56c21da8ff4c854c97f8b4fb
KeoniSoler <keoni@Keonis-MacBook-Pro.local>
2024-11-24 16:30:20 -0300

    Login hecho

--- proyectoProg2/controllers/usersControllers.js
+++ proyectoProg2/controllers/usersControllers.js
@@ -11,7 +11,29 @@ const usersControllers = {
     showlogin: function(req, res){
         return res.render('login');
     },
-
+    login: function(req, res){
+        let form = req.body;
+        let filtrado = {
+            where:{email: form.email}
+        }
+        users.findOne(filtrado)
+        .then(function(results){
+            if(!results){
+                return res.send("Este email no existe");
+            }else{
+                let data = bcryptjs.compareSync(form.password, results.password);
+                if(data){
+                    req.session.user = results.dataValues;
+                    return res.redirect("/products")
+                }else{
+                    return res.send("La contraseña es incorrecta");
+                }
+            }
+        })
+        .catch(function(e){
+            return console.log(e);
+        })
+    },
 };
 
 module.exports = usersControllers;
--- proyectoProg2/routes/users.js
+++ proyectoProg2/routes/users.js
@@ -4,5 +4,6 @@ const usersControllers = require("../controllers/usersControllers");
 
 router.get("/register", usersControllers.showregister);
 router.get("/login", usersControllers.showlogin);
+router.post("/login", usersControllers.login);
 
 module.exports = router;
