b6b8284af326f6cc1643758ebd19aa248950061d
salvadordelosheros <sdelosheros@udesa.edu.ar>
2024-11-24 16:34:01 -0300

    register terminado

--- proyectoProg2/routes/users.js
+++ proyectoProg2/routes/users.js
@@ -5,5 +5,6 @@ const usersControllers = require("../controllers/usersControllers");
 router.get("/register", usersControllers.showregister);
 router.get("/login", usersControllers.showlogin);
 router.post("/login", usersControllers.login);
+router.post("/register", usersControllers.register);
 
 module.exports = router;
